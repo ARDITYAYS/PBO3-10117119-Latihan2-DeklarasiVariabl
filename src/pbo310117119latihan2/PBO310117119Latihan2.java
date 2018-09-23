b82fcb33341091c761ecb6a3fac08c9a4cddabbe
ARDITYAYS <220499yogaputa@gmail.com>
2018-09-23 20:04:43 +0700

    first commit

---
+++ src/pbo310117119latihan2/PBO310117119Latihan2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo310117119latihan2;
+
+/**
+ *
+ * @author user
+ * Nama      : Arditya Yogaputra S
+ * Kelas     : IF-3 / PBO-3
+ * Nim       : 10117119
+ * Deskripsi : Program ini Mencontohkan cara Mendeklarasikan Variabel
+ */
+public class PBO310117119Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Deklarasi Variabel
+    int nilaiInt;
+    final double PHI = 3.14; //konstanta uppercase
+    boolean nilaiLogika;
+    char nilaiKarakter;
+    
+    //Memberi nilai ke variabel
+    nilaiInt = 78;
+    nilaiLogika = false;
+    nilaiKarakter = 'D';
+    
+    //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+    }
+    
+}
